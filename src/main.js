7794cbbc687d6b2a2f07bbafe867a89ba8bf0fe9
LTAO <1718127900@qq.com>
2020-03-12 10:06:03 +0800

    Revert "刘滔的拍卖系统"
    This reverts commit 5d50e1befa331b559ce5a558f7dee77e8eaa29f0.

--- src/main.js
+++ src/main.js
@@ -2,22 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import jQuery from 'jquery'
 
 Vue.config.productionTip = false
-Vue.use(element)
-
-//跨域设置
-jQuery.ajaxSetup({
-    xhrFields:{
-      withCredentials: true
-    },
-    crossDomain: true
-})
-
-Vue.prototype.jquery=jQuery
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import login from '../views/Login.vue'
-import register from '../views/register.vue'
-import index from '../views/index.vue'
-import auction from '../views/auction.vue'
-import manager from '../views/spmanager.vue'
-import adds from '../views/addSp.vue'
-import results from '../views/spResult.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/login',
-    name: 'login',
-    component: login
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/register',
-    name: 'register',
-    component: register
-  },
-  {
-    path: '/index',
-    name: 'index',
-    component: index,
-    children:[
-      {
-        path: '/index/auction',
-        name: 'auction',
-        component: auction
-      },
-      {
-        path: '/index/manager',
-        name: 'manager',
-        component: manager
-      },
-      {
-        path: '/index/adds',
-        name: 'adds',
-        component: adds
-      },
-      {
-        path: '/index/results',
-        name: 'results',
-        component: results
-      },
-    ]
-  },
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ]
 
 const router = new VueRouter({
   routes
-})
-
-//路由前置守卫拦截器
-router.beforeEach((to, from, next) => {
-  //放行登录注册
-  if(to.path === '/login' || to.path === '/register'){
-    next();
-  }else{
-    //已登录放行
-    if(sessionStorage.getItem("users")){
-        next();
-    }else{
-      next('/login');
-    }
-  }
 })
 
 export default router
